1e452e3952f5c1e1d77094c8f380537d45cddbbd
Brenno <brennoogd@gmail.com>
2025-07-03 17:39:59 -0300

    Inicio parte com javascript

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Ativar Links do Menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add('ativo');
+  }
+}
+
+links.forEach(ativarLink);
+
+//Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
